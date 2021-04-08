78d3092267b7694ed22ceac309c1b2507af8560f
Russ <rperry1886@gmail.com>
2021-04-07 20:54:50 -0400

    Add Scenes, Buttons, and Button Script
    Add a Win, Start, and Game Scene. Added a button script that will take you to the next scene (only applies to start page right now.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Need this for scene management
+
+public class SceneLoader : MonoBehaviour {
+    public void LoadNextScene() {
+        // Find the active scene and get its index.
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Load next scene
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
